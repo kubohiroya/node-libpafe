e21290d91ff14fb8d0d7b1489134c087fac5f58c
Hiroya Kubo <kubo.hiroya@gmail.com>
2014-02-09 17:30:36 +0900

    Revert "Revert "Revert "Revert "Revert "remove libpafe"""""
    This reverts commit 2c7620f5b0e17b770f7aaa2cf2827aa00b5a2dfd.

---
+++ deps/unix/libpafe/tests/pasori_test.c
@@ -0,0 +1,106 @@
+/* $Id: pasori_test.c,v 1.5 2009-10-09 07:43:14 hito Exp $ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h"
+#include "libpafe.h"
+
+int
+test(pasori *p)
+{
+  unsigned char data[] = "test data.";
+  int n = sizeof(data) - 1, r = 0;
+
+  printf("Echo test... ");
+  if (pasori_test_echo(p, data, &n)) {
+    r |= 1;
+    printf("error!\n");
+  } else {
+    printf("success\n");
+  }
+
+  printf("EPROM test... ");
+  if (pasori_test_eprom(p)) {
+    r |= 1;
+    printf("error!\n");
+  } else {
+    printf("success\n");
+  }
+
+  printf("RAM test... ");
+  if (pasori_test_ram(p)) {
+    r |= 1;
+    printf("error!\n");
+  } else {
+    printf("success\n");
+  }
+
+  printf("CPU test... ");
+  if (pasori_test_cpu(p)) {
+    r |= 1;
+    printf("error!\n");
+  } else {
+    printf("success\n");
+  }
+
+  printf("Polling test... ");
+  if (pasori_test_polling(p)) {
+    r |= 1;
+    printf("error!\n");
+  } else {
+    printf("success\n");
+  }
+  return r;
+}
+
+void
+show_version(pasori *p)
+{
+  int v1, v2;
+
+  if (pasori_version(p, &v1, &v2)) {
+    printf("cannot get version.\n");
+    return;
+  }
+
+  switch (pasori_type(p)) {
+  case PASORI_TYPE_S310:
+    printf("PaSoRi (RC-S310)\n firmware version %d.%02d\n", v1, v2);
+    break;
+  case PASORI_TYPE_S320:
+    printf("PaSoRi (RC-S320)\n firmware version %d.%02d\n", v1, v2);
+    break;
+  case PASORI_TYPE_S330:
+    printf("PaSoRi (RC-S330)\n firmware version %d.%02d\n", v1, v2);
+    break;
+  default:
+    printf("unknown hardware.\n");
+  }
+
+}
+
+int
+main(void)
+{
+  pasori *p;
+  int type;
+
+  p = pasori_open();
+  if (!p) {
+    printf("error\n");
+    exit(-1);
+  }
+  pasori_init(p);
+
+  show_version(p);
+
+  type = pasori_type(p);
+  if ((type == PASORI_TYPE_S310 || type == PASORI_TYPE_S320) && test(p)) {
+    pasori_close(p);
+    return 1;
+  }
+
+  pasori_close(p);
+
+  return 0;
+}
